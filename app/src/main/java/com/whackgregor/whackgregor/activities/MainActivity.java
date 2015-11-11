9ba1e61d59b1e1261e8fd6b09b4c27f7671a30fe
Carlos Balladares <carlosballadares@Carloss-MacBook-Pro.local>
2015-11-10 20:42:00 -0800

    ui changes and mole figure

--- app/src/main/java/com/whackgregor/whackgregor/activities/MainActivity.java
+++ app/src/main/java/com/whackgregor/whackgregor/activities/MainActivity.java
@@ -4,15 +4,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 import com.whackgregor.whackgregor.R;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+
+    public int[] holes ={R.id.hole1, R.id.hole2, R.id.hole3, R.id.hole4, R.id.hole5, R.id.hole6, R.id.hole7, R.id.hole8, R.id.hole9};
+
+    public int[] moles ={R.drawable.holewithmole, R.drawable.holewithmole2};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView hole = (ImageView) findViewById(holes[5]);
+        hole.setImageResource(R.drawable.holewithmole2);
+
     }
 
     @Override
@@ -35,5 +45,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public int getRandomItem(int[] array){
+        Random rand= new Random();
+        return rand.nextInt(array.length);
     }
 }
