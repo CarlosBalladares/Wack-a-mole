44c40e270fb4919d075555483c879f1bb0ae103d
Carlos Balladares <carlos@balladares.co>
2015-11-11 05:35:39 -0800

    removing stuff

--- app/src/main/java/com/whackgregor/whackgregor/ui/HoleView.java
+++ app/src/main/java/com/whackgregor/whackgregor/ui/HoleView.java
@@ -50,4 +50,6 @@ public class HoleView extends ImageView {
     }
 
 
+
+
 }
--- app/src/main/java/com/whackgregor/whackgregor/ui/MainActivity.java
+++ app/src/main/java/com/whackgregor/whackgregor/ui/MainActivity.java
@@ -4,12 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageView;
+
 
 import com.whackgregor.whackgregor.R;
 
-import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
